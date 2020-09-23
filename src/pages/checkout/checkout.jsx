26d825033e4488816746eab3ab1afcb7f52a56ef
Richard Tang <rxtang@ucsd.edu>
2020-09-23 14:19:52 -0700

    added stripe and updated favicon

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -7,6 +7,7 @@ import { selectCartItems, selectCartItemsTotal } from '../../redux/cart/cart.sel
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/chekout-item';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
             <span> TOTAL: ${total} </span>
         </div>
+        <div className='test-warning'>
+            *Use test credit card for payment*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
